0ade6ea8819fd886389be7f52f466e97e381c123
Yuriy Sosnin <sosnin.yuriy@gmail.com>
2022-12-26 16:46:40 +0300

    Refactor app.py

--- app.py
+++ app.py
@@ -5,8 +5,10 @@ import plotly.express as px
 import pandas as pd
 import geopandas as gpd
 
+
 regions = gpd.read_file('data/regions.geojson')
 data = pd.read_csv('data/interactions.csv')
+
 
 var_map = {
     'pop_total': 'Население',
@@ -48,6 +50,41 @@ var_map = {
     'distance_capitals': 'Расстояние между административными центрами', 
     'distance_centroids': 'Расстояние между центроидами',
 }
+
+sources_text = '''
+* GitHub: https://github.com/yuasosnin/empire-migrations-coursework
+* Источники данных: 
+    * Миграции:
+    
+    Эта работа,
+    
+    Первая всеобщая перепись населения Российской империи 1897 года  / Изд. Центр. Стат. комитетом М-ва вн. дел ; Под ред. Н. А. Тройницкого. - СПб., 1897 - 1905.
+    
+    * Границы на карте, языки: 
+    
+    Sablin, Ivan; Kuchinskiy, Aleksandr; Korobeinikov, Aleksandr; Mikhaylov, Sergey; Kudinov, Oleg; Kitaeva, Yana; Aleksandrov, Pavel; Zimina, Maria; Zhidkov, Gleb, 2015, "Transcultural Empire: Geographic Information System of the 1897 and 1926 General Censuses in the Russian Empire and Soviet Union", https://doi.org/10.11588/data/10064, heiDATA, V3
+    
+    * Занятость по отраслям:
+    
+    http://gpih.ucdavis.edu/
+    
+    * Климатические факторы:
+    
+    Harris, I., Osborn, T.J., Jones, P. et al. Version 4 of the CRU TS monthly high-resolution gridded multivariate climate dataset. Sci Data 7, 109 (2020). https://doi.org/10.1038/s41597-020-0453-3
+    
+    * Доля крепостных:
+    
+    Markevich, A., & Zhuravskaya, E. (2018). The Economic Effects of the Abolition of Serfdom: Evidence from the Russian Empire. American Economic Review, 108(4–5), 1074–1117. 
+    
+    Тройницкий, А. Г. (1861). Крепостное население в России по 10-й народной переписи. Центральный статистический комитет. Статистический отдел.
+    
+    * Все остальнве переменные:
+    
+    Кесслер Хайс и Маркевич Андрей, Электронный архив Российской исторической статистики, XVIII – XXI вв., Режим доступа: https://ristat.org/, Версия I (2020).
+
+* Сделано в Dash, размещено на Heroku
+'''
+
 
 def plot_variable(regions, var, title=False):
     regions['id'] = regions.index
@@ -143,132 +180,79 @@ def plot_region(data, regions, reg_name, how):
     fig.update_traces(showlegend=False)
     return fig
 
+
+### Tab 1
+# to- and from- migrations
+
 how_dropdown = dcc.Dropdown(
     id='how-dropdown', 
     clearable=False,
     value='to', 
-    options=[{'label': l, 'value': v} for l, v in zip(['в', 'из'], ['to', 'from'])]
-)
+    options=[{'label': l, 'value': v} for l, v in zip(['в', 'из'], ['to', 'from'])])
+
 region_dropdown = dcc.Dropdown(
     id='region-dropdown',
     clearable=False,
     value='Томская губерния',
-    options=[{'label': c, 'value': c} for c in sorted(regions.name.unique())]
-)
+    options=[{'label': c, 'value': c} for c in sorted(regions.name.unique())])
 
-tab1_children = dcc.Tab(
-    label='Миграции',
-    children=[
-        html.Div(
-            children=[
-                html.Div(
-                    children=[
-                        html.Div(
-                            children=[
-                                html.Label("Направление"),
-                                how_dropdown,
-                            ],
-                            style=dict(width='50%')
-                        ),
-                        html.Div(
-                            children=[
-                                html.Label("Регион"),
-                                region_dropdown
-                            ],
-                            style=dict(width='50%')
-                        )
-                    ],
-                    #style={'display': 'inline-block', 'vertical-align': 'top'},
-                    style=dict(display='flex')
-                ),
-                html.Div(
-                    children=[
-                        dcc.Graph(id='graph1', config=dict(displayModeBar=False)),
-                    ],
-                    #style={'display': 'inline-block', 'vertical-align': 'top'},
-                ),
-            ],
-            #style=dict()
-        )
-    ]
-)
+how_dropdown_with_label = html.Div(
+    children=(html.Label("Направление"), how_dropdown),
+    style=dict(width='50%'))
 
-color_dropdown = dcc.Dropdown(
+region_dropdown_with_label = html.Div(
+    children=(html.Label("Регион"), region_dropdown),
+    style=dict(width='50%'))
+
+both_dropdowns = html.Div(
+    children=(how_dropdown_with_label, region_dropdown_with_label),
+    style=dict(display='flex'))
+graph1 = dcc.Graph(id='graph1', config=dict(displayModeBar=False))
+
+tab_1 = dcc.Tab(
+    children=(both_dropdowns, graph1),
+    label='Миграции')
+
+
+### Tab 2
+# variables for regions
+
+var_dropdown = dcc.Dropdown(
     id='color-dropdown',
     clearable=False,
     value='pop_total',
-    options=[{'label': v, 'value': k} for k, v in var_map.items() if k in regions.columns]
-)
+    options=[{'label': v, 'value': k} for k, v in var_map.items() if k in regions.columns])
 
-tab2_children = dcc.Tab(
-    label='Региональные переменные', 
-    children=[
-        html.Div(
-            children=[
-                html.Label("Переменная"),
-                color_dropdown,
-            ],
-        ),
-        dcc.Graph(id='graph2', config=dict(displayModeBar=False)),
-    ]
-)
+var_dropdown_with_label = html.Div(
+    children=(html.Label("Переменная"), var_dropdown))
+graph2 = dcc.Graph(id='graph2', config=dict(displayModeBar=False))
 
-tab3_children = dcc.Tab(
-    label='Источники', 
-    children=[
-        html.Div(
-            children=[
-                dcc.Markdown('''
-                * GitHub: https://github.com/yuasosnin/empire-migrations-coursework
-                * Источники данных: 
-                    * Миграции:
-                    
-                    Эта работа,
-                    
-                    Первая всеобщая перепись населения Российской империи 1897 года  / Изд. Центр. Стат. комитетом М-ва вн. дел ; Под ред. Н. А. Тройницкого. - СПб., 1897 - 1905.
-                    
-                    * Границы на карте, языки: 
-                    
-                    Sablin, Ivan; Kuchinskiy, Aleksandr; Korobeinikov, Aleksandr; Mikhaylov, Sergey; Kudinov, Oleg; Kitaeva, Yana; Aleksandrov, Pavel; Zimina, Maria; Zhidkov, Gleb, 2015, "Transcultural Empire: Geographic Information System of the 1897 and 1926 General Censuses in the Russian Empire and Soviet Union", https://doi.org/10.11588/data/10064, heiDATA, V3
-                    
-                    * Занятость по отраслям:
-                    
-                    http://gpih.ucdavis.edu/
-                    
-                    * Климатические факторы:
-                    
-                    Harris, I., Osborn, T.J., Jones, P. et al. Version 4 of the CRU TS monthly high-resolution gridded multivariate climate dataset. Sci Data 7, 109 (2020). https://doi.org/10.1038/s41597-020-0453-3
-                    
-                    * Доля крепостных:
-                    
-                    Markevich, A., & Zhuravskaya, E. (2018). The Economic Effects of the Abolition of Serfdom: Evidence from the Russian Empire. American Economic Review, 108(4–5), 1074–1117. 
-                    
-                    Тройницкий, А. Г. (1861). Крепостное население в России по 10-й народной переписи. Центральный статистический комитет. Статистический отдел.
-                    
-                    * Все остальнве переменные:
-                    
-                    Кесслер Хайс и Маркевич Андрей, Электронный архив Российской исторической статистики, XVIII – XXI вв., Режим доступа: https://ristat.org/, Версия I (2020).
-            
-                * Сделано в Dash, размещено на Heroku
-                '''),
-            ],
-        ),
-    ]
-)
+tab_2 = dcc.Tab(
+    children=(var_dropdown_with_label, graph2),
+    label='Региональные переменные')
+
+### Tab 3
+# sources
+
+tab_3 = dcc.Tab(
+    children=(dcc.Markdown(sources_text)),
+    label='Источники')
+
+
+### App
+
+app_label = html.H1("Миграции в Российской империи, 1897")
+app_tabs = dcc.Tabs(
+    children=(tab_1, tab_2, tab_3))
 
 app = dash.Dash(__name__)
 server = app.server
-app.layout = html.Div(
-    children=[
-        html.H1("Миграции в Российской империи, 1897"),
-        dcc.Tabs([tab1_children, tab2_children, tab3_children])
-    ]
-)
+app.layout = html.Div(children=(app_label, app_tabs))
 
 @app.callback(
     Output('graph1', 'figure'),
-    [Input("region-dropdown", "value"),
-     Input("how-dropdown", "value")]
+    [Input("region-dropdown", "value"), 
+    Input("how-dropdown", "value")]
 )
 def update_figure1(region, how):
     return plot_region(data, regions, region, how)
